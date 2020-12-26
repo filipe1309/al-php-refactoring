56240177a5c493441ffd0c2273aac20ef766075e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-12-26 16:16:07 -0300

    refactor: extract code to functions

--- extrair_funcao/index.php
+++ extrair_funcao/index.php
@@ -5,11 +5,23 @@ $saldos = [
     'Erika' => 5000,
 ];
 
-//Verifica se o usuário existe
-if (array_key_exists("Giovanni", $saldos)) {
-    //Se existir, exibe o saldo na tela
-    echo "<p>O saldo do Giovanni é: ${saldos['Giovanni']}</p>";
-} else {
-    //Se não, exibe erro
+function exibeSaldoCorrentista(string $nome, array $saldos)
+{
+    if (array_key_exists($nome, $saldos)) {
+        exibeSaldo($nome, $saldos);
+    } else {
+        exibeErro();
+    }
+}
+
+function exibeErro()
+{
     echo "<p>Correntista não existente.</p>";
 }
+
+function exibeSaldo(string $nome, array $saldos)
+{
+    echo "<p>O saldo do $nome é: {$saldos[$nome]}</p>";
+}
+
+exibeSaldoCorrentista('Giovanni', $saldos);
