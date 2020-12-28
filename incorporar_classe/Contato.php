e013cc2e29251a9ee3cce9a43879b8c80029e601
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-12-28 00:19:42 -0300

    refactor: extract class  Cep

--- incorporar_classe/Contato.php
+++ incorporar_classe/Contato.php
@@ -7,57 +7,13 @@ namespace Alura\IncorporarClasse;
 class Contato
 {
     private $endereco;
-    private $cep;
-    private $ddd;
-    private $telefone;
-    private $tipoTelefone;
 
-    public function __construct(
-        string $endereco,
-        string $cep,
-        string $ddd,
-        string $telefone,
-        string $tipoTelefone
-    ) {
+    public function __construct(string $endereco) {
         $this->endereco = $endereco;
-        $this->cep = $cep;
-        $this->ddd = $ddd;
-        $this->telefone = $telefone;
-        $this->tipoTelefone = $tipoTelefone;
-    }
-
-    public function getCep(): string
-    {
-        return $this->cep;
-    }
-
-    public function getDdd(): string
-    {
-        return $this->ddd;
-    }
-
-    public function getTelefone(): string
-    {
-        return $this->telefone;
-    }
-
-    public function getTipoTelefone(): string
-    {
-        return $this->tipoTelefone;
     }
 
     public function getEndereco(): string
     {
         return $this->endereco;
-    }
-
-    public function getEnderecoECep(): string
-    {
-        return "$this->endereco $this->cep";
-    }
-
-    public function getTelefoneDdd(): string
-    {
-        return "{$this->getTipoTelefone()}: ({$this->getDdd()}) {$this->getTelefone()}";
     }
 }
