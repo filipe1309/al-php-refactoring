db853aca9f791a39d9f4e7283e4ff84ad47a75af
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-12-28 00:20:55 -0300

    refactor: incorporate class Cep

--- incorporar_classe/Contato.php
+++ incorporar_classe/Contato.php
@@ -7,13 +7,28 @@ namespace Alura\IncorporarClasse;
 class Contato
 {
     private $endereco;
+    private $cep;
 
-    public function __construct(string $endereco) {
+    public function __construct(
+        string $endereco,
+        string $cep
+    ) {
         $this->endereco = $endereco;
+        $this->cep = $cep;
+    }
+
+    public function getCep(): string
+    {
+        return $this->cep;
     }
 
     public function getEndereco(): string
     {
         return $this->endereco;
+    }
+
+    public function getEnderecoECep(): string
+    {
+        return "$this->endereco $this->cep";
     }
 }
